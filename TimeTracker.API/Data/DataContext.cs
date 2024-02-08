8ca09b05455c1ab8caf14e5f472fa244d2647cd9
Chris Brown <cpbrown.tx@gmail.com>
2024-02-08 13:35:46 -0600

    Added User Entity and created Many to Many relationship to Project Entity.

--- TimeTracker.API/Data/DataContext.cs
+++ TimeTracker.API/Data/DataContext.cs
@@ -10,5 +10,6 @@
         public DbSet<TimeEntry> TimeEntries { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<ProjectDetails> ProjectDetails { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
