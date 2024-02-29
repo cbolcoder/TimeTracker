014e40492924a866c6affcd699788bf703d4c399
Chris Brown <cpbrown.tx@gmail.com>
2024-02-29 10:00:55 -0600

    Added Identity services and changed project entity.

--- TimeTracker.API/Data/DataContext.cs
+++ TimeTracker.API/Data/DataContext.cs
@@ -1,6 +1,8 @@
-﻿namespace TimeTracker.API.Data
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace TimeTracker.API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
@@ -8,11 +10,11 @@
         {
             modelBuilder.Entity<TimeEntry>().Navigation(c => c.Project).AutoInclude();
             modelBuilder.Entity<Project>().Navigation(c => c.ProjectDetails).AutoInclude();
+            base.OnModelCreating(modelBuilder);
         }
 
         public DbSet<TimeEntry> TimeEntries { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<ProjectDetails> ProjectDetails { get; set; }
-        public DbSet<User> Users { get; set; }
     }
 }
--- TimeTracker.Shared/Entities/Project.cs
+++ TimeTracker.Shared/Entities/Project.cs
@@ -11,6 +11,5 @@ namespace TimeTracker.Shared.Entities
         public required string Name { get; set; }
         public List<TimeEntry> TimeEntries { get; set; } = new List<TimeEntry>();
         public ProjectDetails? ProjectDetails { get; set; }
-        public List<User> Users { get; set; } = new List<User>();
     }
 }
