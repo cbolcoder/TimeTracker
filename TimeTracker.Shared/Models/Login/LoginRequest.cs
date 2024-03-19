7ffca31c26bcfc948d094f5a33ff81fc0f45f04b
Chris Brown <cpbrown.tx@gmail.com>
2024-03-19 01:27:26 -0500

    Modified login form css

--- TimeTracker.Shared/Models/Login/LoginRequest.cs
+++ TimeTracker.Shared/Models/Login/LoginRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace TimeTracker.Shared.Models.Login
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeTracker.Shared.Models.Login
 {
     public class LoginRequest
     {
+        [Required]
         public string UserName { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = string.Empty;
     }
 }
